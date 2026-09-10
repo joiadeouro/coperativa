aa87111ae1cc65a34389827b5f2f8140d983444f
joiadeouro <amandaangela534@gmail.com>
2026-09-10 10:37:52 -0300

    terceiro

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,7 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBkHt5Hqzg9FxcQXAzyx7M4OFEtmz1nBeo",
-    authDomain: "joiadeouro-15c11.firebaseapp.com",
-    projectId: "joiadeouro-15c11",
+  apiKey: "AIzaSyBkHt5Hqzg9FxcQXAzyx7M4OFEtmz1nBeo",
+  authDomain: "joiadeouro-15c11.firebaseapp.com",
+  projectId: "joiadeouro-15c11",
+  storageBucket: "joiadeouro-15c11.firebasestorage.app",
+  messagingSenderId: "124336415287",
+  appId: "1:124336415287:web:dcc21e4009279e1a79d5b2"
 };
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
